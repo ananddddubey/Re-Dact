fdb8b9a46ba1eec8bdbdb445981d19058418e9ad
Anand <career.anand04@gmail.com>
2024-10-12 00:17:33 +0530

    feat: add deployment port

--- app.py
+++ app.py
@@ -51,4 +51,4 @@ def download_file(filename):
     return send_from_directory(app.config['REDACTED_FOLDER'], filename, as_attachment=True)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port= 8080)
